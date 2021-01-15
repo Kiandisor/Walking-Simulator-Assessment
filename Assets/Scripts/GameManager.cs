9ace622dd6e3c3a3938add9f6a59bd7432179bde
Kiandisor <59274326+Kiandisor@users.noreply.github.com>
2021-01-15 14:13:51 +1100

    Start of the UI
    Created a game manager which creates a list of objects which is passes around to the UI

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    List<GameObject> collectables = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var objects = FindObjectsOfType<GameObject>();
+        foreach(var obj in objects) {
+            if(obj.tag =="Collectables") {
+                collectables.Add(obj);
+			}
+        } 
+    }
+    
+    public List<GameObject> get_collectables()
+	{
+        return collectables;
+	}
+}
