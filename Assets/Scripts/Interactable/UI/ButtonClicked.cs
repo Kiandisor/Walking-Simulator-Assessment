9efb4a02a347c658f8e6242c8a0fa4acb4165511
Kiandisor <mojynardi@gmail.com>
2021-01-21 02:30:53 +1100

    Fixed #5
    Menu buttons now correctly work.
    
    Fixed by using events/delegates
    
    Added a ButtonClicked script

---
+++ Assets/Scripts/Interactable/UI/ButtonClicked.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonClicked : MonoBehaviour
+{
+    private Button current_button;
+    // Start is called before the first frame update
+    void Start()
+    {
+        current_button=gameObject.GetComponent<Button>();
+
+        current_button.onClick.AddListener(run_change_scene);
+    }
+
+    // Update is called once per frame
+    void run_change_scene()
+    {
+        MenusManger.run_menu_function(gameObject.name);
+    }
+}
